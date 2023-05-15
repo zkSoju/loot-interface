62b89526a8463cb6592b19d28747cef288820b64
soju <underrated@gmail.com>
2023-05-15 02:55:35 -0700

    Update Navbar.tsx

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="fixed z-50 w-full px-48">
       <div className="flex w-full items-center justify-center border-b border-white/5 px-8 py-4">
-        <p className="text-base text-black">Snapshotta</p>
+        {/* <p className="text-base text-black">Snapshotta</p> */}
       </div>
     </div>
   );
